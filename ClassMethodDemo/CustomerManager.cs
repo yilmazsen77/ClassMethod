7f59b060730294e79067f506bea57e2725aa0237
Yılmaz Şen <yilmazsen77@outlook.com>
2021-02-07 21:29:08 +0300

    listings

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    public class CustomerManager
+    {
+        List<Customer> customers = new List<Customer>();
+
+        public void Add(Customer customer)
+        {
+            customers.Add(customer);
+        }
+
+        public void Delete(Customer customer)
+        {
+            customers.Remove(customer);
+            Console.WriteLine("Müsteri " + customer.Name + " silindi.");
+        }
+
+        public void List()
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+            }
+            
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer();
+            c1.Id = 1;
+            c1.Name = "a";
+            c1.EMailAdress = "a@a.com";
+
+            Customer c2 = new Customer();
+            c2.Id = 2;
+            c2.Name = "b";
+            c2.EMailAdress = "b@b.com";
+
+            Customer c3 = new Customer();
+            c3.Id = 3;
+            c3.Name = "c";
+            c3.EMailAdress = "c@c.com";
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(c1);
+            customerManager.Add(c2);
+            customerManager.Add(c3);
+
+
+            customerManager.List();
+
+            customerManager.Delete(c1);
+
+            customerManager.List();
+
+            customerManager.Delete(c2);
+
+            customerManager.List();
+        }
+    }
+}
